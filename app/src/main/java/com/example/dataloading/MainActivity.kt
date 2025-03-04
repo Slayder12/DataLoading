59f579492232a167274b04618589ab5f82c25216
Slayder12 <gnutenko.s.r@gmail.com>
2025-03-04 14:34:57 +1000

    update

--- app/src/main/java/com/example/dataloading/MainActivity.kt
+++ app/src/main/java/com/example/dataloading/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.example.dataloading
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -16,21 +42,91 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            MyApp()
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp() {
+    var isSwitchChecked by rememberSaveable { mutableStateOf(false) }
+    var isDataLoaded by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+    val data = "Это пример загруженных данных. Это может быть рассказ, список пользователей и т.д."
+
+    val purpleColor = Color(0xFF664FA3)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "MyProgramm") },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = purpleColor,
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = if (isDataLoaded) data else "Данные не загружены",
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center)
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally),
+                        onClick = {
+                            if (isSwitchChecked) {
+                                isDataLoaded = true
+                            } else {
+                                Toast.makeText(context, "Нет доступа", Toast.LENGTH_SHORT).show()
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = purpleColor)
+                    ) {
+                        Text(text = "Загрузка данных", color = Color.White)
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Switch(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally),
+                        checked = isSwitchChecked,
+                        onCheckedChange = { isSwitchChecked = it }
                     )
                 }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
